df170f5b051b91acafe6a4c8c39d032721524a0e
Steven Lee <stevelee83@gmail.com>
2023-04-18 15:20:55 -0700

    add code for cls SerialGenerator

--- serial.py
+++ serial.py
@@ -1,6 +1,6 @@
 class SerialGenerator:
     """Machine to create unique incrementing serial numbers.
-    
+
     >>> serial = SerialGenerator(start=100)
 
     >>> serial.generate()
@@ -17,3 +17,19 @@ class SerialGenerator:
     >>> serial.generate()
     100
     """
+    def __init__(self, start):
+        """Create generator with start value and sequential value"""
+        self.start = start
+        self.sequential = self.start
+
+    def __repr__(self):
+        return f"<SerialGenerator start={self.start}>"
+
+    def generate(self):
+        """Add +1 to sequential"""
+        self.sequential = self.sequential + 1
+        return self.sequential - 1
+
+    def reset(self):
+        """Reset sequential value to start value"""
+        self.sequential = self.start
